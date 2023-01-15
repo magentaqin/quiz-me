64a3c67578dfe1ec0025ba8b948d17245b6adc0e
qinmu <magenta2127@gmail.com>
2023-01-15 21:20:20 +0800

    feat: list questions

--- frontend/api/question.ts
+++ frontend/api/question.ts
@@ -1,5 +1,25 @@
 import { axiosInstance } from "./axios"
 
+export interface ListQuestionReq {
+  offset: number
+  count: number
+}
+
+export interface ListQuestionRes {
+  title: string
+  description: string
+  questionId: string
+  tags: string[]
+}
+
 export const listTagsApi = async () => {
   return await axiosInstance.get('/tag/list')
+}
+
+export const listQuestionsApi = async (params: ListQuestionReq) => {
+  return await axiosInstance.get('/question/list', { params })
+}
+
+export const countQuestionApi = async () => {
+  return await axiosInstance.get('/question/totalCount')
 }
--- frontend/components/QuizTable.tsx
+++ frontend/components/QuizTable.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState, ChangeEvent, useEffect } from 'react'
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -7,9 +7,11 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
+import { countQuestionApi, listQuestionsApi, ListQuestionRes } from '../api/question'
+import { unEscape } from '../pages/editor/utils/html';
 
 interface Column {
-  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  id: 'title' | 'description';
   label: string;
   minWidth?: number;
   align?: 'right';
@@ -17,83 +19,56 @@ interface Column {
 }
 
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2),
-  },
+  { id: 'title', label: 'Question', minWidth: 150 },
+  { id: 'description', label: 'Description', minWidth: 200 },
 ];
 
-interface Data {
-  name: string;
-  code: string;
-  population: number;
-  size: number;
-  density: number;
-}
+export default function QuizTable() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rowsData, setRowsData] = useState<ListQuestionRes[]>([])
+  const [count, setCount] = useState(0)
 
-function createData(
-  name: string,
-  code: string,
-  population: number,
-  size: number,
-): Data {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-export default function StickyHeadTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event: unknown, newPage: number) => {
+  const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
+    fetchQuestions(rowsPerPage*newPage)
   };
 
-  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
 
+  const fetchQuestions = async (offset: number) => {
+    const res = await listQuestionsApi({
+      offset,
+      count: rowsPerPage
+    })
+    if (res.data) {
+      const questions = res.data.questions.map((item: ListQuestionRes) => {
+        return {
+          ...item,
+          title: unEscape(item.title),
+          description: unEscape(item.description)
+        }
+      })
+      setRowsData(questions)
+    }
+  }
+
+  useEffect(() => {
+    // get the total count of questions
+    countQuestionApi().then(res => {
+      if (res.data) {
+        setCount(res.data.count)
+      }
+    })
+    fetchQuestions(0)
+  }, []);
+
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+      <TableContainer sx={{ height: 600 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
@@ -109,11 +84,10 @@ export default function StickyHeadTable() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            {rowsData
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.questionId}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
@@ -131,9 +105,9 @@ export default function StickyHeadTable() {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+        rowsPerPageOptions={[10, 20, 30]}
         component="div"
-        count={rows.length}
+        count={count}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
