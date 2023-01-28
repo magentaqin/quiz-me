9e6fea681d57eff65b8dbb2cf7311c12e8dfbef7
qinmu <magenta2127@gmail.com>
2023-01-28 23:16:08 +0800

    feat: init UserForm

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,17 +1,34 @@
+import { useState, Fragment } from 'react'
 import Image from 'next/image'
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import styles from '../styles/Home.module.scss'
+import UserForm from './UserForm'
+
+type FormType = 'signup' | 'login' | 'question'
 
 const NavBar = () => {
+    const [formType, setFormType] = useState<FormType>()
+    const [open, setOpen] = useState(false)
+    const handleSignup = () => {
+      setFormType('signup')
+      setOpen(true)
+    }
+    const handleLogin = () => {
+      setFormType('login')
+      setOpen(true)
+    }
     return (
-			<div className={styles.navbar}>
+      <Fragment>
+        <div className={styles.navbar}>
 				<Image src="/logo.png" height={50} width={200} alt="logo" />
 				<Stack direction="row" spacing={2}>
-				  <Button variant="text" size="small">Sign Up</Button>
-				  <Button variant="contained" size="small">Login</Button>
+				  <Button variant="text" size="small" onClick={handleSignup}>Sign Up</Button>
+				  <Button variant="contained" size="small" onClick={handleLogin}>Login</Button>
 				</Stack>
 			</div>
+       <UserForm open={open} setOpen={setOpen} />
+      </Fragment>
     )
 }
 
---
+++ frontend/components/UserForm.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface Props {
+  open: boolean;
+  setOpen: (val: boolean) => void;
+}
+
+export default function UserForm(props: Props) {
+  const { open, setOpen } = props
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
