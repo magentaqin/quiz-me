fbfca8ef89343b9a81ae5d5f7c7648703f9f5cd4
qinmu <magenta2127@gmail.com>
2023-03-28 19:17:30 +0800

    feat: add google analyze script

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,9 +5,26 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DQMJH33LP7"
+        onLoad={() => {
+          (window as any).dataLayer = (window as any).dataLayer || [];
+          function gtag(s1: any, s2: any) {
+            (window as any).dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+
+          gtag("config", "G-DQMJH33LP7");
+        }}
+      />
+    </>
+  );
 }
 
 export default MyApp;
