4632427412ed8738f4d5c767697034d2e917f4c8
qinmu <magenta2127@gmail.com>
2023-05-23 18:00:48 +0800

    feat: update s3 config

--- backend/app/extend/application.ts
+++ backend/app/extend/application.ts
@@ -4,9 +4,11 @@ import { S3Client } from '@aws-sdk/client-s3';
 const prisma = new PrismaClient();
 
 const s3Config = {
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_ACCESS_SECRET,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION || '',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY || '',
+    secretAccessKey: process.env.AWS_ACCESS_SECRET || '',
+  }
 };
 
 const s3Client = new S3Client(s3Config);
