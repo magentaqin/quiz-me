4db247dda9ae0701d500b058f1aa384500f6b5da
qinmu <magenta2127@gmail.com>
2023-05-30 19:53:42 +0800

    fix: fix upload filekey param can not be read

--- frontend/api/upload.ts
+++ frontend/api/upload.ts
@@ -2,8 +2,8 @@ import { axiosInstance } from "./axios";
 
 export const uploadImageApi = async (file: File, fileKey: string) => {
   const formData = new FormData()
-  formData.append('file', file)
   formData.append('fileKey', fileKey)
+  formData.append('file', file)
   return await axiosInstance.post("/upload/image", formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
