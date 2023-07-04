31eb0e7fafd5fe837448e922d649e11e40eefbf8
qinmu <magenta2127@gmail.com>
2023-07-04 08:11:56 +0800

    feat: add log dir for development

--- backend/config/config.local.ts
+++ backend/config/config.local.ts
@@ -2,5 +2,8 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+  config.logger = {
+    dir: `${config.root}/logs/dev/${config.name}`,
+  };
   return config;
 };
