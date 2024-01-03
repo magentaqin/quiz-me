8c6bb4c3ea84d322b019c89f54c2b383a85f4aa5
qinmu <magenta2127@gmail.com>
2024-01-03 16:46:17 +0800

    feat: migrate s3 endpoint to digitalocean

--- backend/app/extend/application.ts
+++ backend/app/extend/application.ts
@@ -4,18 +4,20 @@ import { S3Client } from '@aws-sdk/client-s3';
 const prisma = new PrismaClient();
 
 const s3Config = {
+  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+  endpoint: 'https://sfo3.digitaloceanspaces.com',
   region: process.env.AWS_REGION || '',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY || '',
     secretAccessKey: process.env.AWS_ACCESS_SECRET || '',
-  }
+  },
 };
 
 const s3Client = new S3Client(s3Config);
 
 const extendedApp = {
   prisma,
-  s3Client
+  s3Client,
 };
 
 export default extendedApp;
